0496df7a82fc4ce25e3858dda4cba1c0d86b6c26
Tristan <Triatan.Panggabean@student.rocva.nl>
2021-06-15 14:31:37 +0200

    05 javasript taak04

--- 05 - Javascript/taak04 - String kan plakken/script.js
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -1,5 +1,7 @@
-let mijnVoornaam = "Tristan";
+let mijnVoornaam = prompt("vul je naam in");
 let mijnAchternaam = 'Panggabean';
-let mijnleeftijd = "18 jaar"
+let mijnleeftijd = "18 jaar";
 
-let volledigeNaam = mijnVoornaam + " " +  mijnAchternaam + "" + mijnleeftijd;
+let volledigeNaam = "Mijn naam is " + mijnVoornaam + " " + mijnAchternaam + " en mijn leeftijd is " + mijnleeftijd; 
+
+console.log (volledigeNaam);
